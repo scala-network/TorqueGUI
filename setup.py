2963164c9f6dbe4f592f8e638e8832df46ddeb09
Stellite <35624857+stellitecoin@users.noreply.github.com>
2018-03-09 23:32:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from cx_Freeze import setup,Executable
 
 setup(
 	name = "Stellite GUI",
-	version = "0.0.1",
+	version = "0.0.2",
 	description = "A GUI for the Stellite Cryptocurrency",
 	executables = [Executable("wallet.py", base = "Win32GUI")])

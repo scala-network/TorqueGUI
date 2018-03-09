b6fe9fbc39369d0e1444e76483797244eaa12bf7
Stellite <35624857+stellitecoin@users.noreply.github.com>
2018-03-09 23:35:13 +0530

    Update __init__.py

--- settings/__init__.py
+++ settings/__init__.py
@@ -15,7 +15,7 @@ from utils.common import getHomeDir, makeDir
 
 USER_AGENT = "Stellite GUI Wallet"
 APP_NAME = "Stellite Wallet"
-VERSION = [0, 0, 1]
+VERSION = [0, 0, 2]
 
 
 _data_dir = makeDir(os.path.join(os.getcwd(), 'data'))
